4ce747e676e0ca54dfb8c1c73c99c8a0892f50e3
Francesco Cagnin <francesco.cagnin@gmail.com>
2017-07-26 15:29:17 +0200

    Add helper script for starting uds servers

---
+++ start-gym-uds-servers.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+pkill -f gym-uds-server.py
+sleep 1
+
+GYM_UDS_API_DIR="/opt/gym-uds-api"
+ENV_ID="CartPole-v0"
+N=1
+for ((i=0; i < $N; ++i)); do
+   python3 $GYM_UDS_API_DIR/gym-uds-server.py $ENV_ID /tmp/gym-uds-socket-GA3C-$i &
+done
