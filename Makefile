af46a88537c6d79717c68b555293a8a7b0153fb4
Francesco Cagnin <francesco.cagnin@gmail.com>
2017-07-26 15:43:57 +0200

    Move gym.{h,cc} and gym-uds.pb.{h,cc} to include/third_party

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 default:
 	$(CXX) -std=c++11 -O2 -pthread -o GAC3 \
-	-I include -I include/third_party -l tensorflow_cc src/*.cc
+	-I include -I include/third_party -l tensorflow_cc \
+	include/third_party/gym.cc include/third_party/gym-uds.pb.cc src/*.cc
 
 clean:
 	rm -rf GAC3
--- build.sh
+++ build.sh
@@ -8,17 +8,17 @@ if [[ ! -d $TENSORFLOW_DIR ]] || [[ ! -d $GYM_UDS_API_DIR ]] ; then
     exit 1
 fi
 
-rm -f include/gym-uds.pb.h src/gym-uds.pb.cc
-$TENSORFLOW_DIR/bazel-out/host/bin/external/protobuf/protoc --python_out=$GYM_UDS_API_DIR --proto_path=$GYM_UDS_API_DIR $GYM_UDS_API_DIR/gym-uds.proto
-$TENSORFLOW_DIR/bazel-out/host/bin/external/protobuf/protoc --cpp_out=. --proto_path=$GYM_UDS_API_DIR $GYM_UDS_API_DIR/gym-uds.proto
-mv gym-uds.pb.h include && mv gym-uds.pb.cc src
-cp $GYM_UDS_API_DIR/binding-cpp/include/gym.h include && cp $GYM_UDS_API_DIR/binding-cpp/src/gym.cc src
-
 rm -rf include/third_party
 mkdir include/third_party
+
 cp -r $TENSORFLOW_DIR/tensorflow include/third_party
 cp -r $TENSORFLOW_DIR/bazel-genfiles/tensorflow include/third_party
 cp -r $TENSORFLOW_DIR/bazel-tensorflow/../../external/protobuf/src/google include/third_party
 cp -r $TENSORFLOW_DIR/third_party/eigen3 include/third_party
 cp -r $TENSORFLOW_DIR/bazel-tensorflow/../../external/eigen_archive/. include/third_party/eigen3
 cp -r include/third_party/eigen3/Eigen include/third_party
+
+$TENSORFLOW_DIR/bazel-out/host/bin/external/protobuf/protoc --python_out=$GYM_UDS_API_DIR --proto_path=$GYM_UDS_API_DIR $GYM_UDS_API_DIR/gym-uds.proto
+$TENSORFLOW_DIR/bazel-out/host/bin/external/protobuf/protoc --cpp_out=. --proto_path=$GYM_UDS_API_DIR $GYM_UDS_API_DIR/gym-uds.proto
+mv gym-uds.pb.h include/third_party && mv gym-uds.pb.cc include/third_party
+cp $GYM_UDS_API_DIR/binding-cpp/include/gym.h include/third_party && cp $GYM_UDS_API_DIR/binding-cpp/src/gym.cc include/third_party
