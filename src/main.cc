e868f03a2697b85d934bd7327d93c8e30ba8c7ef
Francesco Cagnin <francesco.cagnin@gmail.com>
2017-07-27 12:32:41 +0200

    Cleanup

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <chrono>
 #include <cmath>
+#include <cstdint>
 #include <deque>
 #include <iostream>
 #include <random>
@@ -16,13 +17,12 @@
 
 #include "gym.h"
 
-
-using experience_t = std::tuple<gym::observation_t, std::vector<float>, float, gym::observation_t, bool>;
+using encoded_action_t = std::vector<float>;
+using experience_t = std::tuple<gym::observation_t, encoded_action_t, float, gym::observation_t, bool>;
 using memory_t = std::deque<experience_t>;
 
 const auto NUM_AGENTS = 1;
 const auto NUM_TRAINING_STEPS = 1000;
-int steps_done = 0;
 
 const auto BATCH_SIZE = 32;
 const auto N_STEP_RETURN = 8;
@@ -34,6 +34,7 @@ const auto EPSILON_END = 0.15f;
 
 Model model;
 Queue<experience_t> experiences_queue;
+int training_steps_done = 0;
 
 std::mt19937 generator(1337);
 
@@ -73,7 +74,7 @@ void update(memory_t& memory, float& R, const experience_t& experience)
             const auto n = memory.size();
             const auto experience = get_sample(memory, R, n);
             experiences_queue.push(experience);
-            ++steps_done;
+            ++training_steps_done;
 
             const auto reward = std::get<2>(memory[0]);
             R = (R - reward) / DISCOUNT;
@@ -85,7 +86,7 @@ void update(memory_t& memory, float& R, const experience_t& experience)
     if (memory.size() >= N_STEP_RETURN) {
         const auto experience = get_sample(memory, R, N_STEP_RETURN);
         experiences_queue.push(experience);
-        ++steps_done;
+        ++training_steps_done;
 
         const auto reward = std::get<2>(memory[0]);
         R = (R - reward);
@@ -95,7 +96,7 @@ void update(memory_t& memory, float& R, const experience_t& experience)
     // possible edge case - if an episode ends in <N steps, the computation is incorrect
 }
 
-gym::action_t pick_action(gym::Environment& env, const std::vector<float>& state, float epsilon)
+gym::action_t pick_action(gym::Environment& env, const gym::observation_t& state, float epsilon)
 {
     if (std::uniform_real_distribution<float>(0.0f, 1.0f)(generator) < epsilon) {
         return env.sample();
@@ -107,7 +108,7 @@ gym::action_t pick_action(gym::Environment& env, const std::vector<float>& state
     }
 }
 
-void agent(unsigned i)
+void agent(uint32_t i)
 {
     auto env = gym::Environment("/tmp/gym-uds-socket-GA3C-" + std::to_string(i));
 
@@ -115,20 +116,20 @@ void agent(unsigned i)
     float R = 0.0f;
 
     const bool render = false;
-    unsigned episode = 0;
+    uint32_t episode = 0;
     while (true) {
         ++episode;
-        std::vector<float> curr_state, next_state;
+        gym::observation_t curr_state, next_state;
         curr_state = env.reset();
 
-        float t = (float)steps_done / (NUM_TRAINING_STEPS-1);
+        float t = (float)training_steps_done / (NUM_TRAINING_STEPS-1);
         float epsilon = (1-t)*EPSILON_START + t*EPSILON_END;
 
         float reward = 0.0f;
         float episode_reward = 0.0f;
         bool done = false;
         while (!done) {
-            if (steps_done > NUM_TRAINING_STEPS) {return;}
+            if (training_steps_done > NUM_TRAINING_STEPS) { return; }
 
             const auto action = pick_action(env, curr_state, epsilon);
             std::tie(next_state, reward, done) = env.step(action);
@@ -169,11 +170,11 @@ void fit(const std::vector<experience_t>& batch)
 
 void trainer()
 {
-    while (steps_done < NUM_TRAINING_STEPS) {
-        const auto batch_size = std::min(BATCH_SIZE, NUM_TRAINING_STEPS-steps_done);
+    while (training_steps_done < NUM_TRAINING_STEPS) {
+        const auto batch_size = std::min(BATCH_SIZE, NUM_TRAINING_STEPS-training_steps_done);
 
         auto batch = std::vector<experience_t>(batch_size);
-        for (int i = 0; i < batch.size(); ++i) {
+        for (auto i = 0; i < batch.size(); ++i) {
             batch[i] = experiences_queue.pop();
         }
         fit(batch);
@@ -190,16 +191,16 @@ int main(int argc, char const *argv[])
 
     // start the agents
     auto agents_threads = std::vector<std::thread>(NUM_AGENTS);
-    for (int i = 0; i < NUM_AGENTS; ++i) {
+    for (auto i = 0; i < NUM_AGENTS; ++i) {
         agents_threads[i] = std::thread(agent, i);
     }
 
     // and wait for them to finish
     trainer_thread.join();
-    for (int i = 0; i < NUM_AGENTS; ++i) {
+    for (auto i = 0; i < NUM_AGENTS; ++i) {
         agents_threads[i].join();
     }
 
     // save the trained model
-    // model.save();
+    model.save();
 }
