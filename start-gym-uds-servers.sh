1e28171995ee1317e97dae4e64cce9637f34bf7a
Francesco Cagnin <francesco.cagnin@gmail.com>
2017-07-28 14:25:26 +0200

    Fix to use gym-uds-api submodule

--- start-gym-uds-servers.sh
+++ start-gym-uds-servers.sh
@@ -2,9 +2,8 @@
 pkill -f gym-uds-server.py
 sleep 1
 
-GYM_UDS_API_DIR="/opt/gym-uds-api"
 ENV_ID="CartPole-v0"
 N=5
 for ((i=0; i < $N; ++i)); do
-   python3 $GYM_UDS_API_DIR/gym-uds-server.py $ENV_ID /tmp/gym-uds-socket-GA3C-$i &
+    python3 gym-uds-api/gym-uds-server.py $ENV_ID /tmp/gym-uds-socket-GA3C-$i &
 done
