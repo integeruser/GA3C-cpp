ec285ff67f5915fb5b9b12c6a207986f43f21e4a
Francesco Cagnin <francesco.cagnin@gmail.com>
2020-02-29 12:02:03 +0100

    Update script to use python instead of python3

--- cartpole-v0/start-gym-uds-servers.sh
+++ cartpole-v0/start-gym-uds-servers.sh
@@ -6,5 +6,5 @@ cd $(dirname $(realpath $0))
 ENV_ID="CartPole-v0"
 N=5
 for ((i=0; i < $N; ++i)); do
-    python3 third-party/gym-uds-api/gym-uds-server.py $ENV_ID /tmp/gym-uds-socket-GA3C-$i &
+    python third-party/gym-uds-api/gym-uds-server.py $ENV_ID /tmp/gym-uds-socket-GA3C-$i &
 done
